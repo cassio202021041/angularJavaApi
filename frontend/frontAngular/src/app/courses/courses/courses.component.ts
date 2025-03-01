5a061dd0fe5a3acdd0e2d989b0dca2585edb2861
cassio202021041 <cassio00710@hotmail.com>
2025-03-01 13:12:26 -0300

    Sexta aula

--- frontend/frontAngular/src/app/courses/courses/courses.component.ts
+++ frontend/frontAngular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Frontend'},
+    {_id: "1", name: 'React', category: 'Frontend'},
+    {_id: "2", name: 'Vue', category: 'Frontend'},
+    {_id: "3", name: 'Node', category: 'Backend'},
+    {_id: "4", name: 'Express', category: 'Backend'},
+    {_id: "5", name: 'Django', category: 'Backend'},
+    {_id: "6", name: 'Flask', category: 'Backend'},
+  ];
+  displayedColumns = ['name', 'category'];
+
+
+  constructor() {
+    // this.courses = [];
+  }
+
+  ngOnInit(): void {
+  }
 
 }
