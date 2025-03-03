4662126679debcc9dd9af8838b16e537e68f38a3
cassio202021041 <cassio00710@hotmail.com>
2025-03-03 18:29:20 -0300

    setimaAula- Fim aula 9

--- frontend/frontAngular/src/app/courses/services/courses.service.ts
+++ frontend/frontAngular/src/app/courses/services/courses.service.ts
@@ -2,24 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = './assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return  [
-      {_id: '1', name: 'Angular', category: 'Frontend'},
-      {_id: "1", name: 'React', category: 'Frontend'},
-      {_id: "2", name: 'Vue', category: 'Frontend'},
-      {_id: "3", name: 'Node', category: 'Backend'},
-      {_id: "4", name: 'Express', category: 'Backend'},
-      {_id: "5", name: 'Django', category: 'Backend'},
-      {_id: "6", name: 'Flask', category: 'Backend'},
-    ];
-
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
